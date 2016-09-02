a1d076dd0e2a44bfcba283e4dad3f2a8f0bf29ba
Vishal Patel <vpatel589@csu.fullerton.edu>
2016-09-02 11:27:40 -0700

    fixed grouplayout bug

--- View.java
+++ View.java
@@ -11,13 +11,13 @@ public class View{
 		mainFrame = new JFrame("Address Book Proggie");
 		mainFrame.setSize(600,300);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setVisible(true);
 
 		tabs = new JTabbedPane();
 		tabs.addTab("Search", new SearchTab());
 		tabs.addTab("Insert", new InsertTab("This is the insert tab!"));
 		tabs.addTab("Delete", new DeleteTab("This is the tab used to delete items from the database"));
 		mainFrame.add(tabs);
+		mainFrame.setVisible(true);
 		
 	}
 
@@ -45,7 +45,7 @@ class InsertTab extends JPanel{
 class SearchTab extends JPanel{
 	public SearchTab(){
 		//give this panel a border)
-		this.setBorder(BorderFactory.createMatteBorder(10,10,10,10, Color.BLUE));
+ 		this.setBorder(BorderFactory.createMatteBorder(10,10,10,10, Color.BLUE));
 
 		//set the group layout
 		GroupLayout gl = new GroupLayout(this);
@@ -54,39 +54,45 @@ class SearchTab extends JPanel{
 
 		//create labels..		
 		JLabel searchLabel = new JLabel("Search by...");
-//		JLabel nameLabel = new JLabel("Name: ");
+		JLabel nameLabel = new JLabel("Name: ");
 //		JLabel addressLabel = new JLabel("Address: ");
 //		JLabel phoneLabel = new JLabel("Phone Number: ");
-/*
-		//create text fields...
-		JTextField nameField = new JTextField(20);
-		JTextField addressField = new JTextField(20);
-		JTextField phoneField = new JTextField(20);
 
-*/
+		//create text fields...
+		JTextField searchField = new JTextField(10);
+		JTextField nameField = new JTextField(20);
+//		JTextField addressField = new JTextField(20);
+//		JTextField phoneField = new JTextField(20);
+
+
 		gl.setHorizontalGroup(gl.createSequentialGroup()
 			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addComponent(searchLabel))
-/*			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addComponent(nameLabel)
-				.addComponent(addressLabel)
-				.addComponent(phoneLabel))		
-*/
+				.addComponent(searchLabel)
+				.addComponent(nameLabel))
+//			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
+//				.addComponent(nameLabel))
+//				.addComponent(addressLabel)
+//				.addComponent(phoneLabel))		
+			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addComponent(searchField)
+				.addComponent(nameField))
 );
 
 		gl.setVerticalGroup(gl.createSequentialGroup()
 			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				.addComponent(searchLabel))
-/*			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				.addComponent(searchLabel)
+				.addComponent(searchField))
+			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
 				.addComponent(nameLabel)
 				.addComponent(nameField))
-			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				.addComponent(addressLabel)
-				.addComponent(addressField))
-			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				.addComponent(phoneLabel)
-				.addComponent(phoneField))
-*/
+//			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
+//				.addComponent(addressLabel)
+//				.addComponent(addressField))
+//			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
+//				.addComponent(phoneLabel)
+//				.addComponent(phoneField))
+//
+
 );
 System.out.println("Hello there");		
 
